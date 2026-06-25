8228d6e8f1c4540486ecc7465d212450ab3ad5a5
Tran Dong <trandong2932002@gmail.com>
2026-06-26 05:20:15 +0700

    nvim: set show symbols keymap

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -10,6 +10,7 @@ return {
     vim.keymap.set("n", "<leader>fg", builtin.live_grep)
     vim.keymap.set("n", "<leader>fb", builtin.buffers)
     vim.keymap.set("n", "<leader>fd", builtin.diagnostics)
+    vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols)
 
     require("telescope").load_extension("fzf")
   end
