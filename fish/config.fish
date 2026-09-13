deebb4aa3c531273f179f0bfcdae852fa5fe63ff
Tran Dong <trandong2932002@gmail.com>
2026-09-13 16:18:21 +0700

    fish: refactor

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,4 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
 end
 
 set -U fish_greeting
@@ -9,22 +8,21 @@ set -x PNPM_HOME "$HOME/.local/share/pnpm"
 fish_add_path "$PNPM_HOME/bin"
 
 # dotnet
-set -x DOTNET_CLI_TELEMETRY_OPTOUT 1 # stupid telemetry
-set -l DOTNET_ROOT "$HOME/.local/share/dotnet/dotnet-current"
+set -x DOTNET_CLI_TELEMETRY_OPTOUT 1
+set -x DOTNET_ROOT "$HOME/.local/share/dotnet/dotnet-current"
 set -l DOTNET_TOOL "$HOME/.dotnet/tools"
 fish_add_path "$DOTNET_ROOT"
 fish_add_path "$DOTNET_TOOL"
 
 # uv 
-set -x UV_INSTALL_DIR "$HOME/.local/share/uv" # installation only
-set -x UV_TOOL_DIR "$UV_INSTALL_DIR/tools"
-set -x UV_TOOL_BIN_DIR "$UV_INSTALL_DIR"
-set -x UV_PYTHON_BIN_DIR "$UV_INSTALL_DIR/python"
-fish_add_path "$UV_INSTALL_DIR"
+set -l UV_HOME "$HOME/.local/share/uv"
+set -x UV_TOOL_DIR "$UV_HOME/tools"
+set -x UV_TOOL_BIN_DIR "$UV_HOME/bin"
 fish_add_path "$UV_TOOL_BIN_DIR"
 
 # go
-set -x GOROOT "$HOME/.local/share/golang/go-current"
-set -x GOPATH "$GOROOT/packages"
-fish_add_path "$GOROOT/bin"
+set -x GOPATH "$HOME/.local/share/go"
 fish_add_path "$GOPATH/bin"
+
+# rust
+set -x RUSTUP_HOME "$HOME/.local/rustup"
