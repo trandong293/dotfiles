0bb849c65941b2e8449ecea19ebde3b92479af17
Tran Dong <trandong2932002@gmail.com>
2026-09-17 11:56:24 +0700

    fish: fix envs

--- fish/config.fish
+++ fish/config.fish
@@ -9,8 +9,11 @@ fish_add_path "$PNPM_HOME/bin"
 
 # dotnet
 set -x DOTNET_CLI_TELEMETRY_OPTOUT 1
-set -x DOTNET_ROOT "$HOME/.local/share/dotnet/dotnet-current"
-set -l DOTNET_TOOL "$HOME/.dotnet/tools"
+set -x DOTNET_NOLOGO 1
+set -x DOTNET_CLI_HOME "$HOME/.local/share/dotnet"
+set -x DOTNET_ROOT "$DOTNET_CLI_HOME/dotnet-current"
+set -x NUGET_PACKAGES "$DOTNET_CLI_HOME/nuget/packages"
+set -l DOTNET_TOOL "$DOTNET_CLI_HOME/.dotnet/tools"
 fish_add_path "$DOTNET_ROOT"
 fish_add_path "$DOTNET_TOOL"
 
@@ -25,4 +28,5 @@ set -x GOPATH "$HOME/.local/share/go"
 fish_add_path "$GOPATH/bin"
 
 # rust
-set -x RUSTUP_HOME "$HOME/.local/rustup"
+set -x RUSTUP_HOME "$HOME/.local/share/rustup"
+set -x CARGO_HOME "$HOME/.local/share/cargo"
