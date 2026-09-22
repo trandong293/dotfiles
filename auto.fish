0922b077be7463f270b56f442e688adc6ed66c8c
Tran Dong <trandong2932002@gmail.com>
2026-09-23 03:19:00 +0700

    fix .config already exist

--- auto.fish
+++ auto.fish
@@ -28,8 +28,13 @@ echo_bold 'CONFIGS'
 cd
 
 echo '(1/3) Cloning dotfiles'
-git clone --recurse-submodules https://codeberg.org/trandong293/dotfiles .config
+git clone --recurse-submodules https://codeberg.org/trandong293/dotfiles
+mv dotfiles/* .config
+mv dotfiles/.* .config
+rm -r dotfiles
+cd .config
 git remote set-url origin ssh://git@codeberg.org/trandong293/dotfiles
+cd
 
 echo '(2/3) Creating symlink .bash_profile'
 ln -sf ~/.config/.bash_profile ~/.bash_profile
